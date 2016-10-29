6cd2236ce6a4ad8c2915547ef16eb39d1697d44d
codeworm96 <codeworm96@outlook.com>
2016-10-29 14:12:48 +0800

    [lab4] basic env

--- lab4/env.c
+++ lab4/env.c
@@ -4,4 +4,30 @@
 #include "types.h"
 #include "env.h"
 
-/*Lab4: Your implementation of lab4*/
+
+E_enventry E_VarEntry(Ty_ty ty)
+{
+    E_enventry res = checked_malloc(sizeof(struct E_enventry_));
+    res->kind = E_varEntry;
+    res->u.var.ty = ty;
+    return res;
+}
+
+E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result)
+{
+    E_enventry res = checked_malloc(sizeof(struct E_enventry_));
+    res->kind = E_funEntry;
+    res->u.fun.formals = formals;
+    res->u.fun.result = result;
+    return res;
+}
+
+S_table E_base_tenv(void)
+{
+    return S_empty();
+}
+
+S_table E_base_venv(void)
+{
+    return S_empty();
+}
