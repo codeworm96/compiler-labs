44fd68aa3b7062ee40915e22e74757371fe7cdf4
codeworm96 <codeworm96@outlook.com>
2016-11-02 21:38:04 +0800

    [lab4] add std lib

--- lab4/env.c
+++ lab4/env.c
@@ -7,7 +7,7 @@
 
 E_enventry E_VarEntry(Ty_ty ty)
 {
-    E_enventry res = checked_malloc(sizeof(struct E_enventry_));
+    E_enventry res = checked_malloc(sizeof(*res));
     res->kind = E_varEntry;
     res->u.var.ty = ty;
     return res;
@@ -15,7 +15,7 @@ E_enventry E_VarEntry(Ty_ty ty)
 
 E_enventry E_FunEntry(Ty_tyList formals, Ty_ty result)
 {
-    E_enventry res = checked_malloc(sizeof(struct E_enventry_));
+    E_enventry res = checked_malloc(sizeof(*res));
     res->kind = E_funEntry;
     res->u.fun.formals = formals;
     res->u.fun.result = result;
@@ -32,5 +32,16 @@ S_table E_base_tenv(void)
 
 S_table E_base_venv(void)
 {
-    return S_empty();
+    S_table res = S_empty();
+    S_enter(res, S_Symbol("print"), E_FunEntry(Ty_TyList(Ty_String(), NULL), Ty_Void()));
+    S_enter(res, S_Symbol("flush"), E_FunEntry(NULL, Ty_Void()));
+    S_enter(res, S_Symbol("getchar"), E_FunEntry(NULL, Ty_String()));
+    S_enter(res, S_Symbol("ord"), E_FunEntry(Ty_TyList(Ty_String(), NULL), Ty_Int(0)));
+    S_enter(res, S_Symbol("chr"), E_FunEntry(Ty_TyList(Ty_Int(0), NULL), Ty_String()));
+    S_enter(res, S_Symbol("size"), E_FunEntry(Ty_TyList(Ty_String(), NULL), Ty_Int(0)));
+    S_enter(res, S_Symbol("substring"), E_FunEntry(Ty_TyList(Ty_String(), Ty_TyList(Ty_Int(0), Ty_TyList(Ty_Int(0), NULL))), Ty_String()));
+    S_enter(res, S_Symbol("concat"), E_FunEntry(Ty_TyList(Ty_String(), Ty_TyList(Ty_String(), NULL)), Ty_String()));
+    S_enter(res, S_Symbol("not"), E_FunEntry(Ty_TyList(Ty_Int(0), NULL), Ty_Int(0)));
+    S_enter(res, S_Symbol("exit"), E_FunEntry(Ty_TyList(Ty_Int(0), NULL), Ty_Void()));
+    return res;
 }
