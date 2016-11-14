084a71f6acb60992a204fdc45d235bfd2d58986f
codeworm96 <codeworm96@outlook.com>
2016-11-14 20:09:03 +0800

    [lab5] init

---
+++ lab5/semant.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "util.h"
+#include "errormsg.h"
+#include "symbol.h"
+#include "absyn.h"
+#include "types.h"
+#include "env.h"
+#include "temp.h"
+#include "tree.h"
+#include "frame.h"
+#include "semant.h"
+
+/*Lab4: Your implementation of lab4*/
+F_fragList SEM_transProg(A_exp exp){
+	return NULL;
+}
+
---
+++ lab5/translate.h
@@ -0,0 +1,14 @@
+#ifndef TRANSLATE_H
+#define TRANSLATE_H
+
+/* Lab5: your code below */
+
+typedef struct Tr_exp_ *Tr_exp;
+
+typedef struct Tr_access_ *Tr_access;
+
+typedef struct Tr_accessList_ *Tr_accessList;
+
+typedef struct Tr_level_ *Tr_level;
+
+#endif
