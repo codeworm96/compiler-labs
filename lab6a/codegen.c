311d0b8e715818010cd05a55f069e2f4fb552127
codeworm96 <codeworm96@outlook.com>
2016-11-28 23:17:54 +0800

    [lab6a] init

---
+++ lab6a/codegen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
+#include "symbol.h"
+#include "absyn.h"
+#include "temp.h"
+#include "errormsg.h"
+#include "tree.h"
+#include "assem.h"
+#include "frame.h"
+#include "codegen.h"
+#include "table.h"
+
+//Lab 6: your code here
+AS_instrList F_codegen(F_frame f, T_stmList stmList) {
+}
+
+
---
+++ lab6a/translate.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "util.h"
+#include "table.h"
+#include "symbol.h"
+#include "absyn.h"
+#include "temp.h"
+#include "tree.h"
+#include "printtree.h"
+#include "frame.h"
+#include "translate.h"
+
+struct Tr_access_ {
+	//Lab5: your code here
+};
+
+
+struct Tr_accessList_ {
+	Tr_access head;
+	Tr_accessList tail;	
+};
+
+
+
+struct Tr_level_ {
+	//Lab5: your code here
+};
+
+struct Tr_exp_ {
+	//Lab5: your code here
+};
+
+
